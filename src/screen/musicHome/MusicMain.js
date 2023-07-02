e858c3ece2e196b926a727ec26a0062d02883dfb
Loshith C H <loshith.ch@stackontech.com>
2023-07-02 21:49:45 +0530

    new screens created

---
+++ src/screen/musicHome/MusicMain.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MusicMain = () => {
+  return (
+    <View>
+      <Text>MusicHome</Text>
+    </View>
+  )
+}
+
+export default MusicMain
+
+const styles = StyleSheet.create({})
