e0c7da3eecfb674e508e2f87f840a43fc7aa40bf
Loshith C H <loshith.ch@stackontech.com>
2023-12-24 21:40:08 +0530

    songs added

---
+++ src/constants/audio/Song.js
@@ -0,0 +1,13 @@
+export default{
+    SONG_1:require('../../assets/songs/Aag Udi Victory Anthem Tejas 128 Kbps.mp3'),
+    SONG_2:require('../../assets/songs/Bang Bang Benny Dayal 128 Kbps.mp3'),
+    SONG_3:require('../../assets/songs/Boom Padi Maja Ma 128 Kbps.mp3'),
+    SONG_4:require('../../assets/songs/Ek Nazar Mein Bhi - Taxi No. 9211 128 Kbps.mp3'),
+    SONG_5:require('../../assets/songs/Mere Thumake Ka Kartoot 128 Kbps.mp3'),
+    SONG_6:require('../../assets/songs/Meri Sazaa Vadh 128 Kbps.mp3'),
+    SONG_7:require('../../assets/songs/Nachoge Toh Bachoge Rocket Gang 128 Kbps.mp3'),
+    SONG_8:require('../../assets/songs/Pathaans Theme Pathaan 128 Kbps.mp3'),
+    SONG_9:require('../../assets/songs/Patla Dupatta Md Desi Rockstar 128 Kbps.mp3'),
+    SONG_10:require('../../assets/songs/Tum Bhi Raahi Mili 128 Kbps.mp3'),
+
+};
